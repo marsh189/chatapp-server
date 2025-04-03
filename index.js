c0b424cd2ba6da0f1a7a39067034b4e9382697ca
Matthew Marshall <marsh189@mail.chapman.edu>
2025-04-03 15:00:52 -0700

    fixing cors

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const io = new Server(expressServer, {
   cors: {
     origin:
       process.env.NODE_ENV === 'production'
-        ? false
+        ? '*'
         : ['http://localhost:5173', 'http://127.0.0.1:5173'],
   },
 });
